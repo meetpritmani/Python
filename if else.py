b7faab4e3e9b3b783f2ebc7cd37f3d07107cbc4a
meetpritmani <53914104+meetpritmani@users.noreply.github.com>
2020-05-03 19:26:39 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,11 @@
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+if b > a:
+    print("b is greater than a")
+
+elif a == b:
+    print("a and b are equal")
+
+else:
+    print("a is greater than b")
